7870b7fe0db84ed81298d38eb3aea26a39f6b484
stallone-dev <stallone.developer@gmail.com>
2023-11-14 00:57:33 -0300

    chore(API/routes): Improve delete validations

--- src/server/routes/delete.js
+++ src/server/routes/delete.js
@@ -1,26 +1,29 @@
 "use strict";
 
 import { once } from "node:events";
-import { _validateToken } from "../util.js";
+import { _saveDBData, _validateID, _validateToken } from "../util.js";
 import { TEMPORARY_DB } from "../../db/temporary-db.js";
 
 async function deleteTask(request, response) {
     const { id } = JSON.parse(await once(request, "data"));
 
-    if(!id || !_validateToken(request.headers)){
+    const validate_id = _validateID(id);
+    const validate_auth = _validateToken(request.headers);
+
+    if(!validate_id.result || !validate_auth.result){
         response.writeHead(400);
 
-        let error = "";
-        !id ? error = "Invalid ID"
-            : error = "Not authorized";
+        let error = {};
 
-        response.end(
-            JSON.stringify({error: error})
-        );
+        if(!validate_id.result){error["id"] = validate_id.error;}
+        if(!validate_auth.result){error["auth"] = validate_auth.error;}
+
+        response.end(JSON.stringify({error: error}));
         return;
     }
 
     _deleteTask(id);
+    _saveDBData(TEMPORARY_DB);
 
     response.writeHead(200);
     response.end(JSON.stringify({result: "Task deleted", task_id:id}));
