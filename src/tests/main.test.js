912e1bf822f6462176740fcdf16a5e2cd7d276ff
stallone-dev <stallone.developer@gmail.com>
2023-11-15 12:48:49 -0300

    test(API): Add  validation tests

--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -8,7 +8,7 @@ const _URL = "http://localhost:3000";
 
 describe("API workflows", () => {
     let _server = {};
-    let _global_task_test = {};
+    let _global_task_test = () => {return TEMPORARY_DB[~~(Math.random() * TEMPORARY_DB.length)];};
 
     // Init and finalize tests
     before(async () => {
@@ -188,7 +188,6 @@ describe("API workflows", () => {
 
             deepStrictEqual(req_status.every(e => e === 200), true);
             deepStrictEqual(res_results.every(e => e ==="Post created"), true);
-            _global_task_test = TEMPORARY_DB[~~(Math.random() * TEMPORARY_DB.length)];
         });
 
     });
@@ -261,7 +260,7 @@ describe("API workflows", () => {
 
         it("Update random task", async () => {
             const data = {
-                id: _global_task_test.id,
+                id: _global_task_test().id,
                 title: "UPDATE THIS TASK",
                 description: "RANDOM TEXT TO TEST"
             };
@@ -273,7 +272,7 @@ describe("API workflows", () => {
             const response = await request.json();
 
             const consult_data = Object.entries(JSON.parse(_getDBData()));
-            const modified_data = [...consult_data.filter(item => item[1].id === _global_task_test.id)];
+            const modified_data = [...consult_data.filter(item => item[1].id === data.id)];
 
             deepStrictEqual(request.status, 200);
             deepStrictEqual(response.result, "Task updated");
@@ -284,7 +283,7 @@ describe("API workflows", () => {
 
         it("Update random task with 'completed' param", async () => {
             const data = {
-                id: _global_task_test.id,
+                id: _global_task_test().id,
                 title: "RESOLVE THIS TASK",
                 description: "RANDOM FINAL TEXT TO TEST",
                 completed: true,
@@ -297,13 +296,217 @@ describe("API workflows", () => {
             const response = await request.json();
 
             const consult_data = Object.entries(JSON.parse(_getDBData()));
-            const modified_data = [...consult_data.filter(item => item[1].id === _global_task_test.id)];
+            const modified_data = [...consult_data.filter(item => item[1].id === data.id)];
 
             deepStrictEqual(request.status, 200);
             deepStrictEqual(response.result, "Task updated");
             deepStrictEqual(modified_data[0][1].title, data.title);
             deepStrictEqual(modified_data[0][1].description, data.description);
             notDeepStrictEqual(modified_data[0][1].completed, null);
+        });
+
+        it("Invalid ID - Not defined ID in JSON", async () => {
+            const data = {
+                id__: _global_task_test().id,
+                title: "UPDATE THIS TASK",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.id, "Not find JSON ID key");
+        });
+
+        it("Invalid ID - empty", async () => {
+            const data = {
+                id: "",
+                title: "UPDATE THIS TASK",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.id, "ID empty");
+        });
+
+        it("Invalid ID - Not String", async () => {
+            const data = {
+                id: Number(String(_global_task_test().id).replace("-","")),
+                title: "UPDATE THIS TASK",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.id, "ID is not string");
+        });
+
+        it("Invalid ID - Size incorrectly", async () => {
+            const data = {
+                id: String(_global_task_test().id).replace("-",""),
+                title: "UPDATE THIS TASK",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.id, "ID is not correctly size");
+        });
+
+        it("Invalid DATA - Not defined task title", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title_: "UPDATE THIS TASK",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Not find JSON title key");
+        });
+
+        it("Invalid DATA - Not defined task description", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "UPDATE THIS TASK",
+                description_: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Not find JSON description key");
+        });
+
+        it("Invalid DATA - Title empty", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "",
+                description: "RANDOM TEXT TO TEST"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Title empty");
+        });
+
+        it("Invalid DATA - Description empty", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "UPDATE THIS TASK",
+                description: ""
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Description empty");
+        });
+
+        it("Invalid DATA - Title is not string", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: 123,
+                description: "UPDATE"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Title is not string");
+        });
+
+        it("Invalid DATA - Description is not string", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "UPDATE THIS TASK",
+                description: 123
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Description is not string");
+        });
+
+        it("Invalid DATA - Title is to long", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "MY LONG TEST TITLE FOR TEST ON THIS NODEJS TEST RUNNER, PROBABLY IT'S SO COOL ON REAL APLICATIONS",
+                description: "update"
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Title is to long, max: 60 chars");
+        });
+
+        it("Invalid DATA - Description is to long", async () => {
+            const data = {
+                id: _global_task_test().id,
+                title: "UPDATE THIS TASK",
+                description: "Lorem ipsum dolor sit amet. Est voluptas nisi sit corrupti omnis qui necessitatibus repudiandae est rerum error non veritatis aperiam id nemo dicta! Est impedit tenetur in fuga quaerat ut omnis molestiae. Et maiores doloremque sit quae maiores id tempore laudantium est eligendi deserunt ab enim corrupti. Aut accusantium obcaecati qui molestiae atque non quos quam.Ab voluptatum voluptatem nam voluptas tempora qui quisquam voluptas. Aut accusantium soluta ab voluptate ipsa eos optio repudiandae qui animi labore. Et quisquam vero id quaerat distinctio et ducimus accusantium est galisum fugiat sed eveniet quod qui dolores molestias ut ratione perferendis."
+            };
+
+            const request = await fetch(`${_URL}/tasks/put`, {
+                method: "POST",
+                body: JSON.stringify(data)
+            });
+            const response = await request.json();
+
+            deepStrictEqual(request.status, 400);
+            deepStrictEqual(response.error.data, "Description is to long, max: 300 chars");
         });
 
     });
